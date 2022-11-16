76e6d35702bb5d4e970bda7cf3a215b6c06a44cd
tao <i@taozhengtao.com>
2022-11-16 11:07:52 +0800

    get number form string

---
+++ src/main/java/com/utaoo/string/StringNumUtils.java
@@ -0,0 +1,30 @@
+package com.utaoo.string;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * string and number utils
+ */
+public class StringNumUtils {
+    /**
+     * get all char of number  from string
+     *
+     * @param str any string
+     * @return number in string
+     */
+    public static String getNumber(String str) {
+        String res = "";
+        if (StringUtils.isNotBlank(str)) {
+            str = str.trim();
+            Pattern p = Pattern.compile("\\d{1}");
+            Matcher m = p.matcher(str);
+            while (m.find()) {
+                res += m.group();
+            }
+        }
+        return res;
+    }
+}
